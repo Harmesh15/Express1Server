8b830032b12e37ff72e25ace40764d24c7273b5b
Harmesh15 <b180050@nitsikkim.ac.in>
2026-01-05 18:38:01 +0530

    custome middleware example

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("Server is up and running on port 3000! Ready to handle requests.");
+
+function addproperity(req,res,next){
+    req.user = "Guest";
     next();
+
+}
+
+
+app.get("/welcome",addproperity,(req,res)=>{
+
+    let getvalue = req.user;
+    res.send(`<h1>Welcome, ${getvalue}</h1>`);
+    
 })
 
-app.listen(3000);
+app.listen(3000,()=>{
+    console.log("server stated running at 3000")
+});
